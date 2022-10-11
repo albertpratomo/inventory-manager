d0ac16e2ee2ae3f1a4a3e1e18c13305b03dafae7
albertpratomo <albertpratomo21@gmail.com>
2022-10-11 22:10:32 +0200

    Create InventoryMovement model

---
+++ database/factories/InventoryMovementFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\InventoryMovement>
+ */
+class InventoryMovementFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'quantity' => fake()->numberBetween(1, 10),
+            'unit_price' => (int) fake()->numerify('##0'),
+            'remaining_quantity' => 0,
+        ];
+    }
+}
---
+++ database/migrations/2022_10_11_195519_create_inventory_movements_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventory_movements', function (Blueprint $table) {
+            $table->id();
+            $table->integer('quantity');
+            $table->integer('unit_price'); // In cents.
+            $table->integer('remaining_quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventory_movements');
+    }
+};
