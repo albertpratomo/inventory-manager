2767025be1ae301f3de60f4d6a60d9cdf1bf8fef
albertpratomo <albertpratomo21@gmail.com>
2022-10-20 17:30:24 +0200

    Install Vite and Typescript

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import {defineConfig} from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost',
+        },
+    },
+    plugins: [
+        laravel({
+            input: 'resources/vue/app.ts',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': '/resources/vue',
+        },
+    },
+});
