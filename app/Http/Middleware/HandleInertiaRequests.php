d5afb85894fea7468f25b356a8b2bf83a8efa251
albertpratomo <albertpratomo21@gmail.com>
2022-10-20 15:58:39 +0200

    Convert errors key to camelCase

---
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Request;
+use Inertia\Middleware;
+
+class HandleInertiaRequests extends Middleware
+{
+    /**
+     * Resolves and prepares validation errors in such
+     * a way that they are easier to use client-side.
+     *
+     * @return object
+     */
+    public function resolveValidationErrors(Request $request)
+    {
+        $errors = parent::resolveValidationErrors($request);
+
+        return array_keys_convert_case((array) $errors, 'camel');
+    }
+}
